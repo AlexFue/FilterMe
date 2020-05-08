6782795cb5e3382831bb48615868aa252bb17e7a
Alex Espinoza-Fuentes <alexespinoza11f@gmail.com>
2020-05-07 21:39:49 -0700

    added a few more filters to do

--- filter.py
+++ filter.py
@@ -7,7 +7,13 @@ from PIL import Image
 
 class Filter:
 	def find_filter(self, audio):
-		dic = {'red' : cv2.COLOR_BGR2GRAY}
+		dic = {'red' : cv2.COLOR_BGR2GRAY,
+			   'black and white' : cv2.COLOR_BGR2GRAY,
+			   'alien' : cv2.COLOR_BGR2HLS,
+			   'beach' : cv2.COLOR_BGR2LUV,
+			   'lime' : cv2.COLOR_BGR2Lab,
+			   'black and white times 4' : cv2.COLOR_BGR2YUV_I420,
+			   'blueface' : cv2.COLOR_RGB2BGRA}
 		default = 'did not find it here'
 		for key, value in dic.items():
 			if audio == key:
