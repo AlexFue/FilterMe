371566a0987e3165d623ff76525b5205105fb1bf
Eric Chavez <ericchavez@LC-Mac.local>
2020-05-08 23:49:57 -0700

    First set of color overlays working

--- camera.py
+++ camera.py
@@ -3,57 +3,79 @@ import cv2
 from flask import Flask, render_template, Response
 
 
-
-# class VideoCamera(object):
-#     def __init__(self):
-#         self.video = cv2.VideoCapture(0)
-#     def __del__(self):
-#         self.video.release()
-#     def get_frame(self):
-#         ret, frame = self.video.read()
-#         # DO WHAT YOU WANT WITH TENSORFLOW / KERAS AND OPENCV
-#         frame = cv2.flip(frame,1)
-#         # color_gray = cv2.COLOR_BGR2GRAY
-#         # gray = cv2.cvtColor(frame, color_gray)
-#         ret, jpeg = cv2.imencode('.jpg', frame)
-#         return jpeg.tobytes()
-
+def apply(applyfilter, frame):
+    if applyfilter == 'apply negative':
+        return cv2.bitwise_not(frame)
+    if applyfilter == 'apply Gray':
+        return cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
 class FilterCamera(object):
     def __init__(self, filter):
         self.video = cv2.VideoCapture(0)
         global filters
         filters = filter
+        print(filters)
+        global list_filters
+        global non_filter
+        list_filters = ['6', '52', '50', '44', '128', '2']
+        non_filter = ['apply negative', 'apply Gray',]
+
     def __del__(self):
-        self.video.release()
+       self.video.release()
+
     def get_frame(self):
         ret, frame = self.video.read()
+
         # DO WHAT YOU WANT WITH TENSORFLOW / KERAS AND OPENCV
-        if filters != 'none':
+        if str(filters) in list_filters:
             frame = cv2.flip(frame,1)
             user_filter = filters
             color_cam = cv2.cvtColor(frame, user_filter)
             ret, jpeg = cv2.imencode('.jpg', color_cam)
             return jpeg.tobytes()
-        else:
+        elif filters in non_filter:
+            print("Gray was found")
+            frame = cv2.flip(frame,1)
+            user_filter = apply(filters, frame)
+            ret, jpeg = cv2.imencode('.jpg', user_filter)
+            return jpeg.tobytes()
+        elif filters == 'none':
             frame = cv2.flip(frame,1)
             ret, jpeg = cv2.imencode('.jpg', frame)
             return jpeg.tobytes()
 
-# class FilterCamera(object):
-#     def __init__(self, filter):
-#         self.video = cv2.VideoCapture(0)
-#         global filters
-#         filters = filter
-#     def __del__(self):
-#         self.video.release()
-#     def get_frame(self):
+# def get_frame(self):
+#         # Default camera frame
 #         ret, frame = self.video.read()
-#         # DO WHAT YOU WANT WITH TENSORFLOW / KERAS AND OPENCV
 #         frame = cv2.flip(frame,1)
-#         user_filter = filters
-#         color_cam = cv2.cvtColor(frame, user_filter)
-#         ret, jpeg = cv2.imencode('.jpg', color_cam)
+# ​
+#         ret, frame_red = self.video.read()
+#         frame_red = cv2.flip(frame_red,1)
+# ​
+#         ret, frame_blue = self.video.read()
+#         frame_blue = cv2.flip(frame_blue,1)
+# ​
+#         ret, frame_green = self.video.read()
+#         frame_green = cv2.flip(frame_green,1)
+# ​
+#         ret, frame_yellow = self.video.read()
+#         frame_yellow = cv2.flip(frame_yellow,1)
+# ​
+#         ret, frame_orange = self.video.read()
+#         frame_orange = cv2.flip(frame_orange,1)
+# ​
+#         ret, frame_purple = self.video.read()
+#         frame_purple = cv2.flip(frame_purple,1)
+# ​
+#         filter_dict = {
+#             'apply_negative' : ,
+#             'apply_gray' : ,
+#             'apply_red' : cv2.addWeighted((40,40,240,1), .25, frame_red, .89, 0, frame_red),
+#             'apply_blue' : cv2.addWeighted((255,0,0,1), .25, frame_blue, .89, 0, frame_blue),
+#             'apply_green' : cv2.addWeighted((0,255,60,1), .25, frame_green, .89, 0, frame_green),
+#             'apply_yellow' : cv2.addWeighted((0,255,255,1), .25, frame_yellow, .89, 0, frame_yellow),
+#             'apply_orange' : cv2.addWeighted((0,162,255,1), .25, frame_orange, .89, 0, frame_orange),
+#             'apply_purple' : cv2.addWeighted((125,0,128,1), .25, frame_purple, .89, 0, frame_purple)
+#         }
+#         ret, jpeg = cv2.imencode('.jpg', filter_dict['apply_purple'])
 #         return jpeg.tobytes()
-
-
--- filter.py
+++ filter.py
@@ -3,28 +3,19 @@ import cv2
 from flask import Flask, render_template, Response
 from PIL import Image
 
-
-
 class Filter:
 	def find_filter(self, audio):
-		dic = {'red' : cv2.COLOR_BGR2GRAY,
-			   'black and white' : cv2.COLOR_BGR2GRAY,
+		dic = {'Gray' : cv2.COLOR_BGR2GRAY,
 			   'alien' : cv2.COLOR_BGR2HLS,
-			   'beach' : cv2.COLOR_BGR2LUV,
+			   'Beach' : cv2.COLOR_BGR2LUV,
 			   'lime' : cv2.COLOR_BGR2Lab,
-			   'black and white times 4' : cv2.COLOR_BGR2YUV_I420,
-			   'blueface' : cv2.COLOR_RGB2BGRA}
+			   'black and white x 4' : cv2.COLOR_BGR2YUV_I420,
+			   'smurf' : cv2.COLOR_RGB2BGRA, 
+			   'apply negative' : 'apply negative',
+               'apply Gray' : 'apply Gray'}
 		default = 'did not find it here'
 		for key, value in dic.items():
 			if audio == key:
 				return value
 				break
 		return default
-
-
-
-
-
-
-
-
