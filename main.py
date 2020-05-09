42a36e78dccffeb6fbbb05981baa3c52d323f98f
Eric Chavez <ericchavez@LC-Mac.local>
2020-05-09 12:58:25 -0700

    Finished 99.9% of back-end

--- main.py
+++ main.py
@@ -5,65 +5,64 @@ from camera import FilterCamera
 from speech import Speech
 from filter import Filter
 
+
 app = Flask(__name__)
-#function that generates the camera on the webpage
+# Function that generates the camera on the webpage
 def gen(camera):
 	while True:
 		frame = camera.get_frame()
 		yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
+# Function that generates the filtered camera on the webpage
 def filter_gen(camera, filter):
 	while True:
 		frame = camera.get_frame()
 		yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
-
-#this is the homepage route
+# This is the homepage route
 @app.route('/')
 def home():
 	#renders the homepage template
     return render_template('homepage.html')
 
-
-#this is the secondpage route
+# This is the secondpage route
 @app.route('/second')
 def second():
 	#renders the second page template
     return render_template('secondpage.html')
 
+# This is the page with the filter route
+@app.route('/filters_page')
+def filters_page():
+	# Render the filtered page template
+	return render_template('filters.html')
 
-#this is the video feed route that plays on the second page 
+# This is the video feed route that plays on the second page 
 @app.route('/video_feed')
 def video_feed():
-    # return Response(Camera().gen(),
-    #                 mimetype='multipart/x-mixed-replace; boundary=frame')
-    #shows the video camera on the second page
+    # Shows the video camera on the second page
     videostream = FilterCamera('none')
     return Response(gen(videostream), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-
-#this is the audio route and plays when user clicks to record themselves saying the filter
-@app.route('/audio')
-def audio():
-	#stores what user said in var
-	var = Speech().audio()
-
-	
-		
-	#var goes into find filter function and returns the found filter into choosenfilter
-	choosenfilter = Filter().find_filter(var)
-	filtervideostream = FilterCamera(choosenfilter)
+# This uses what the users choice in order to apply the filtered video
+@app.route('/filter_feed')
+def filter_feed():
 	if choosenfilter != 'did not find it here':
-		#shows the camera with the new filter
-		return Response(gen(filtervideostream), mimetype='multipart/x-mixed-replace; boundary=frame')
+		# Displays the camera with the new filter
+		return Response(gen(filtervideostream), mimetype='multipart/x-mixed-replace; boundary=frame') 
 	else:
-		return choosenfilter
+		# If the user choice is not found then it returns a default camera
+		videostream = FilterCamera('none')
+		return Response(gen(videostream), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-
-
-
-
-
-
-
-
+# This is the audio route and plays when user clicks to record themselves saying the filter
+@app.route('/audio')
+def get_audio():
+	# Stores what user said in var
+	var = Speech().audio()
+	# Var goes into find filter function and returns the found filter into choosenfilter
+	global choosenfilter
+	choosenfilter = Filter().find_filter(var)
+	global filtervideostream
+	filtervideostream = FilterCamera(choosenfilter)
+	return render_template('filters.html')
